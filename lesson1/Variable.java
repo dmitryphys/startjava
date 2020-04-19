4f198fe167f9eeef7f78747b7c3e1f39fcacacd0
Dmitry Morozov <dmitry_phys@mail.ru>
2020-04-19 15:38:17 +0300

    Homework1

---
+++ lesson1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+	public static void main (String[] args) {
+	byte coreCount = 4;
+	short systemType = 64;
+	int frequency = 2100000000;
+	long cpu = 4000000000l;
+	float memorySpace = 1.7f;
+	double freeMemory = 0.123;
+	char systemName = 'W';
+	boolean is7 = true;
+	System.out.println(coreCount);
+	System.out.println(systemType);
+	System.out.println(frequency);
+	System.out.println(cpu);
+	System.out.println(memorySpace);
+	System.out.println(freeMemory);
+	System.out.println(systemName);
+	System.out.println(is7);
+	}
+}

765425a2c7ea1fa5f0b9dfc8ff2b968e32b0647a
Dmitry Morozov <dmitry_phys@mail.ru>
2020-04-25 15:54:36 +0300

    Homework3 (for, while, do while)

---
+++ lesson1/Cycle.java
@@ -0,0 +1,24 @@
+public class Cycle{
+	public static void main(String[] args){
+		for(int i = 0; i <= 20; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		int j = 6;
+		while(j >= -6) {
+			System.out.print(j + " ");
+			j -= 2;
+		}
+		System.out.println();
+		int res = 0;
+		int k = 11;
+		do {
+			if(k % 2 !=0) {
+				res +=k;
+			}
+			k++;
+		}
+		while(k <= 20);
+		System.out.println(res);
+	}
+}
